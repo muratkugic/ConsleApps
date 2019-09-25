e82ec8d31eaf7fa7f7e8f099fa3fc66f6cec4969
muratkugic <m.kugic@gmail.com>
2019-09-25 09:42:38 +0200

    Added Linkz console app

---
+++ Linkz/ConsoleUI/Entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Entry
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Key { get; set; }
+
+        public Entry(string url, string name, string key)
+        {
+            Url = url;
+            Name = name;
+            Key = key;
+        }
+    }
+}
---
+++ Linkz/ConsoleUI/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Entry[] entries = UrlEntries;
+
+            DisplayMenu(entries);
+
+            string userInput = GetUserInput("Enter your option: ");
+
+            ValidateUserEntry(entries, userInput);
+        }
+
+        private static void ValidateUserEntry(Entry[] entries, string userInput)
+        {
+            bool foundEntry = false;
+
+            foreach (var entry in entries)
+            {
+                if (userInput == entry.Key || userInput == entry.Name)
+                {
+                    try
+                    {
+                        System.Diagnostics.Process.Start(@"C:\Program Files\Mozilla Firefox\firefox.exe", entry.Url);
+                        foundEntry = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You don't seem to have Firefox installed...");
+                        Console.WriteLine("Please install the browser and try again.");
+                    }
+                }
+            }
+
+            if (foundEntry == false)
+            {
+                Console.WriteLine("\nCan't find a valid entry. Try again?");
+                Console.WriteLine("\ty - Yes");
+                Console.WriteLine("\tn - No");
+
+                string userChoice = GetUserInput("\nOption: ");
+
+                if (userChoice.ToLower() == "y" || userChoice.ToLower() == "yes")
+                {
+                    RestartApp();
+                }
+            }
+        }
+
+        private static string GetUserInput(string message)
+        {
+            Console.Write(message);
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        private static void DisplayMenu(Entry[] entries)
+        {
+            Console.WriteLine("Enter a letter and press Enter");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"\t{ entry.Key } - { entry.Name }");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static Entry[] UrlEntries => new Entry[]
+        {
+            new Entry("www.twitter.com", "Twitter", "t"),
+            new Entry("www.google.com", "Google", "g"),
+            new Entry("www.youtube.com", "YouTube", "y"),
+            new Entry("www.stackoverflow.com", "Stackoverflow", "s")
+        };
+
+        static void RestartApp()
+        {
+            Console.Clear();
+            Main(null);
+        }
+    }
+}
