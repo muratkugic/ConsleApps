a4e6139a8cf4e8920026692e4e2ae845cacea0f8
muratkugic <m.kugic@gmail.com>
2019-09-30 12:02:45 +0200

    Added latest project

---
+++ GradeBook/GradeBook/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new DiskBook("Feinmann's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named { book.Name }");
+            Console.Clear();
+            Console.WriteLine($"Lowest Grade: { stats.Low }");
+            Console.WriteLine($"Highest Grade: { stats.High }");
+            Console.WriteLine($"Grade average: {stats.Average }");
+            Console.WriteLine($"The letter grade is { stats.Letter }");
+            Console.WriteLine();
+            Console.WriteLine(@"FILE LOCATION: C:\dev\ConsleApps\GradeBook\GradeBook\bin\Debug\netcoreapp3.0");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
+            {
+                Console.Write("Enter a grade or 'q' to quit: ");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+    }
+}
