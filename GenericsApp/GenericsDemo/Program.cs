1bce0929805239021d307890bb5b44a3caf6ec8c
muratkugic <m.kugic@gmail.com>
2019-10-18 14:20:32 +0200

    Added a basics about generics app

---
+++ GenericsApp/GenericsDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = FizzBuzz("tests");
+            Console.WriteLine($"Tests: { result }");
+
+            result = FizzBuzz(123);
+            Console.WriteLine($"123: { result }");
+
+            result = FizzBuzz(true);
+            Console.WriteLine($"True: { result }");
+
+            result = FizzBuzz(new PersonModel { FirstName = "Murat", LastName = "Kugic" });
+            Console.WriteLine($"PersonModel: { result }");
+
+            result = FizzBuzz("testtesttesting");
+            Console.WriteLine($"Testtesttesting: { result }");
+        }
+
+        private static string FizzBuzz<T>(T item)
+        {
+            int itemLength = item.ToString().Length;
+            string output = "";
+
+            if (itemLength % 3 == 0)
+            {
+                output += "Fizz";
+            }
+
+            if (itemLength % 5 == 0)
+            {
+                output += "Buzz";
+            }
+
+            return output;
+        }
+    }
+}
